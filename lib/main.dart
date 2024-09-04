bfdc7fe0f46b2b74076db9ca52b2e52ead40995c
Subrata Singha <subratachandrasingha@gmail.com>
2024-09-04 04:20:33 -0700

    commit one

---
+++ lib/main.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapps/screen2.dart';
+
+main() {
+  runApp(const MyFirstApp());
+
+  List<String> users =[
+    'Rahim',
+    'Karim',
+    'Lalu',
+  ];
+
+}
+
+class MyFirstApp extends StatelessWidget {
+  const MyFirstApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Screen2(),
+        theme:ThemeData(
+            useMaterial3: false
+        )
+    );
+  }
+}
+
+String hello =
+    'Hello World 2 reajkjkfdhjsahf jdhfjaks  jakj fk';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        leading: Icon(Icons.menu,color: Colors.white,),
+        title: Text('title',style: TextStyle(color: Colors.white),),
+        actions: [
+          Icon(Icons.add_a_photo),
+          Icon(Icons.add_a_photo),
+          Icon(Icons.add_a_photo),
+          Icon(Icons.add_a_photo),
+        ],
+      ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              hello,
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700),
+              textAlign: TextAlign.left,
+            ),
+            const SizedBox(height: 50,),
+            Text(hello),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(400)),
+                border: Border.all(color: Colors.red,width: 5),
+                color: Colors.blue.shade500,
+              ),
+              margin: EdgeInsets.all(34),
+              padding: EdgeInsets.symmetric(horizontal:11),
+              height: 100,
+              width: double.infinity,
+              child: Text(hello,style: const TextStyle(color: Colors.white),),
+            ),
+            Icon(Icons.add_circle,color: Colors.blue,size: 122,),
+            Image.network("https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2,height",height: 100,),
+            ClipOval(child: Image.asset("assets/su.jpg",height: 150,)),
+            ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                child: Image.asset("assets/su.jpg",height: 150,)),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen2.dart
@@ -0,0 +1,197 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Screen2 extends StatelessWidget {
+  Screen2({super.key});
+
+  TextEditingController emailTEController = TextEditingController();
+  GlobalKey<FormState> fromkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      endDrawer:Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+                padding: EdgeInsets.zero,
+                margin: EdgeInsets.zero,
+                child: Column(
+                  children: [
+                    ClipRect(
+                        child: Image.network('https://cdn.pixabay.com/photo/2024/05/26/10'
+                            '/15/bird-8788491_1280.jpg',height: 100,errorBuilder: (_,__,___){
+                          return Icon(Icons.image,size: 34,);
+                        },)),
+                    Text('Subrata Singha'),
+                    Text('subratachandrasingha@gmail',),
+                  ],
+                )),
+            Column(
+              children: [
+                ListTile(
+                  tileColor: Colors.amber,
+                  onTap: (){
+                    Navigator.pop(context);
+                  },
+                  leading: Icon(Icons.home_max),
+                  title: Text("home"),
+                  subtitle: Text('ksjfggjghj'),
+                  trailing: Icon(Icons.arrow_forward_ios_rounded),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home_max),
+                  title: Text("home"),
+                  subtitle: Text('ksjfggjghj'),
+                  trailing: Icon(Icons.arrow_forward_ios_rounded),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home_max),
+                  title: Text("home"),
+                  subtitle: Text('ksjfggjghj'),
+                  trailing: Icon(Icons.arrow_forward_ios_rounded),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home_max),
+                  title: Text("home"),
+                  subtitle: Text('ksjfggjghj'),
+                  trailing: Icon(Icons.arrow_forward_ios_rounded),
+                ),
+              ],
+            )
+          ],
+        ),
+      ) ,
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+                padding: EdgeInsets.zero,
+                margin: EdgeInsets.zero,
+                child: Column(
+                  children: [
+                    ClipRect(
+                        child: Image.network('https://cdn.pixabay.com/photo/2024/05/26/10'
+                            '/15/bird-8788491_1280.jpg',height: 100,errorBuilder: (_,__,___){
+                          return Icon(Icons.image,size: 34,);
+                        },)),
+                    Text('Subrata Singha'),
+                    Text('subratachandrasingha@gmail',),
+                  ],
+                )),
+            Column(
+              children: [
+                ListTile(
+                  tileColor: Colors.amber,
+                  onTap: (){
+                    Navigator.pop(context);
+                  },
+                  leading: Icon(Icons.home_max),
+                  title: Text("home"),
+                  subtitle: Text('ksjfggjghj'),
+                  trailing: Icon(Icons.arrow_forward_ios_rounded),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home_max),
+                  title: Text("home"),
+                  subtitle: Text('ksjfggjghj'),
+                  trailing: Icon(Icons.arrow_forward_ios_rounded),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home_max),
+                  title: Text("home"),
+                  subtitle: Text('ksjfggjghj'),
+                  trailing: Icon(Icons.arrow_forward_ios_rounded),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home_max),
+                  title: Text("home"),
+                  subtitle: Text('ksjfggjghj'),
+                  trailing: Icon(Icons.arrow_forward_ios_rounded),
+                ),
+              ],
+            )
+          ],
+        ),        ),
+
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Form(
+              key: fromkey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return "Type your mail";
+                      }
+                    },
+                    controller: emailTEController,
+                    maxLines: null,
+                    style: GoogleFonts.poppins(),
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.account_circle),
+                      suffixIcon: Icon(Icons.remove_red_eye),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(width: 2),
+                      ),
+                      hintText: 'Type your email',
+                    ),
+                  ),
+                  ElevatedButton(onPressed: (){
+                    if(
+                    fromkey.currentState!.validate()){
+                      emailTEController.clear();
+                    };
+                  }, child: Text("Submit")),
+                  Text("Subrata Singha",style: GoogleFonts.poppins(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w900,
+                  ),),
+                  SizedBox(
+                      height: 50,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            foregroundColor: Colors.white,
+                          ),
+                          onPressed: (){}, child: Text("Button",))),
+                  TextButton(onPressed: (){}, child: Text("Button 2")),
+                  OutlinedButton(onPressed: (){}, child: Text("OutlineButton")),
+
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.green,
+          onPressed:(){
+
+            showModalBottomSheet(isScrollControlled: true,
+                context: context,
+                builder: (context){
+                  return Container(
+                    height: 800,
+                  );
+                });
+
+          },child: Icon(Icons.add)),
+    );
+  }
+}
+
+
+
+
